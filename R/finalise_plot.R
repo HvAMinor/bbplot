00bbb043ba14b96db656d75572598de559c96231
HvAMinor <47674357+HvAMinor@users.noreply.github.com>
2019-04-08 13:55:24 +0200

    Update finalise_plot.R

--- R/finalise_plot.R
+++ R/finalise_plot.R
@@ -19,7 +19,7 @@ create_footer <- function (source_name, logo_image_path) {
   #Make the footer
   footer <- grid::grobTree(grid::linesGrob(x = grid::unit(c(0, 1), "npc"), y = grid::unit(1.1, "npc")),
                            grid::textGrob(source_name,
-                                          x = 0.001, hjust = 1, gp = grid::gpar(fontsize=12)),
+                                          x = 0.001, hjust = -0.01, gp = grid::gpar(fontsize=12)),
                            grid::rasterGrob(png::readPNG(logo_image_path), x = 0.92))
   return(footer)
 
