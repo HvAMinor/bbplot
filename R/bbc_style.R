1c7c007964274020b8d6ce8c631493f2a149b832
HvAMinor <47674357+HvAMinor@users.noreply.github.com>
2019-04-08 13:31:46 +0200

    Update bbc_style.R

--- R/bbc_style.R
+++ R/bbc_style.R
@@ -42,7 +42,7 @@ bbc_style <- function() {
   #This sets the text font, size and colour for the axis test, as well as setting the margins and removes lines and ticks. In some cases, axis lines and axis ticks are things we would want to have in the chart - the cookbook shows examples of how to do so.
   axis.title = ggplot2::element_blank(),
   axis.text = ggplot2::element_text(family=font,
-                           size=16,
+                           size=12,
                            color="#222222"),
   axis.text.x = ggplot2::element_text(margin=ggplot2::margin(5, b = 10)),
   axis.ticks = ggplot2::element_blank(),
@@ -60,6 +60,6 @@ bbc_style <- function() {
 
   #Strip background (#This sets the panel background for facet-wrapped plots to white, removing the standard grey ggplot background colour and sets the title size of the facet-wrap title to font size 22)
   strip.background = ggplot2::element_rect(fill="white"),
-  strip.text = ggplot2::element_text(size  = 20,  hjust = 0)
+  strip.text = ggplot2::element_text(size  = 12,  hjust = 0)
   )
   }
